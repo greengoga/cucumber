17fe6f743fc71e4274161d6e1bb5bf3f1e7d0f5c
O.Gogol <o.gogol91@gmail.com>
2025-01-31 10:49:25 +0000

    minor fixes#2

--- src/test/java/ru/netology/RunCucumberTest.java
+++ src/test/java/ru/netology/RunCucumberTest.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {"pretty", "summary"},
-        features = {"src/test/resources/features"},
+        features = {"src/test/resources/"},
         glue = {"ru.netology.steps"})
 
 public class RunCucumberTest {
